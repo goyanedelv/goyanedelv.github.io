7e56f47896bfd4493043ffd08c66949bc5adac64
goyanedelv <goyanedelv@gmail.com>
2025-02-05 18:23:15 -0300

    haciendo que funcione react

---
+++ guagua/src/index.js
@@ -0,0 +1,12 @@
+
+import BabyNameVoting from "./components/BabyNameVoting";
+
+function App() {
+  return (
+    <div className="App">
+      <BabyNameVoting />
+    </div>
+  );
+}
+
+export default App;
